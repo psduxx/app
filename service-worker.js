c39f96aa2c83ca3cf76f293d6a16635de7526e7e
psduxx <dutony131@gmail.com>
2025-05-04 20:34:19 +1000

    update

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,27 @@
-self.addEventListener('install', function (event) {
-  console.log('Service Worker installing.');
+const CACHE_NAME = "tomatoe-app-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+  // 可根据需要加入模型等文件路径
+];
+
+// 安装阶段缓存文件
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('Service Worker activating.');
+// 响应请求
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
